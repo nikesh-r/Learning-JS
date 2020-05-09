d731496467515dc24a4d0b35b24d7ad0f7158fd5
Nikesh <nikesh.ramnani@gmail.com>
2020-05-10 02:06:57 +0530

    fixes

--- 8_todo.js
+++ 8_todo.js
@@ -1,11 +1,11 @@
-addItem = (item) => {
+const addItem = (item) => {
     console.log("Adding item: " + item);    
     todo.push(item);
     console.log("Updated List: ");
     listItem();
 }
 
-deleteItem = (item) => {
+const deleteItem = (item) => {
     console.log("Deleting item: " + item);
     let i = todo.indexOf(item);
     todo.splice(i, 1);
@@ -13,13 +13,13 @@ deleteItem = (item) => {
     listItem();
 }
 
-listItem = () => {
+const listItem = () => {
     for (let i = 0; i < todo.length; i++) {
         console.log((i+1) + " " + todo[i]);
     }
 }
 
-todo = ["world", "fruits", "hello", "groceries"];
+const todo = ["world", "fruits", "hello", "groceries"];
 
 listItem();
 addItem("work");
