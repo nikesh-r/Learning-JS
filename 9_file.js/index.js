c2b0e2bd42da0fbe07547b5f25cb345c54deb4c4
Nikesh <nikesh.ramnani@gmail.com>
2020-05-10 01:47:54 +0530

    add 9_file.js

---
+++ 9_file.js/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const person = {
+    name : "",
+    age : 0,
+    isRacist : false,
+    address : {
+        city : "",
+        country : "",
+        pincode : 0
+    },
+    hobbies: []
+};
+
+person.name = "nick";
+person.age = 20;
+person.address.city = "paris";
+person.hobbies.push("Playing VideoGames");
+
+console.log(person);
+
+let jsonContent = JSON.stringify(person);   //converting js object to json string
+console.log(jsonContent);
+ 
+
+//Saving JSON String content to a json file
+fs.writeFile("outputPerson.json", jsonContent, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+ 
+    console.log("JSON file has been saved.");
+});
